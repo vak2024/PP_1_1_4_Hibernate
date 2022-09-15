3e26c86217cc1d3fba35346c1e16ad00cc41a37b
vak2024 <vak.sar@list.ru>
2022-09-15 09:36:16 +0300

    test

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,12 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.util.Util;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        Util.getConnection();
+       
     }
 }
