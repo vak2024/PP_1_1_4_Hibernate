8126d93b0cd2d3b19cb82c35917603efa1e9d858
vak2024 <vak.sar@list.ru>
2022-09-28 18:59:41 +0300

    corrected the comments

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,10 +1,8 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.dao.UserDao;
-import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
-import jm.task.core.jdbc.util.Util;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -13,14 +11,14 @@ public class Main {
         userService.createUsersTable();
 
         userService.saveUser("Vlad", "Ivanov", (byte) 15);
-//        userService.saveUser("Name2", "LastName2", (byte) 25);
-//        userService.saveUser("Name3", "LastName3", (byte) 31);
-//        userService.saveUser("Name4", "LastName4", (byte) 38);
+        userService.saveUser("Name2", "LastName2", (byte) 25);
+        userService.saveUser("Name3", "LastName3", (byte) 31);
+        userService.saveUser("Name4", "LastName4", (byte) 38);
 
-  //      userService.removeUserById(3);
-//        System.out.println(userDao.getAllUsers());
-//        userService.cleanUsersTable();
-//        userService.dropUsersTable();
+        userService.removeUserById(3);
+        System.out.println(userService.getAllUsers());
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
 
     }
 }
